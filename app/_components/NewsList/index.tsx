eff7c02abd0c3fa5a618bd4ec6db5f3c30ea1756
mkdoi <z.tujing@gmail.com>
2026-04-26 23:17:24 +0900

    5章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -18,13 +18,23 @@ export default function NewsList({ news }: Props) {
           {news.map((article) => (
             <li key={article.id} className={styles.list}>
               <Link href={`/news/${article.id}`} className={styles.link}>
-                <Image
-                  className={styles.image}
-                  src="/no-image.png"
-                  alt="No Image"
-                  width={1200}
-                  height={630}
-                />
+                {article.thumbnail ? (
+                  <Image
+                    className={styles.image}
+                    src={article.thumbnail.url}
+                    alt=""
+                    width={article.thumbnail.width}
+                    height={article.thumbnail.height}
+                  />
+                ) : (
+                  <Image
+                    className={styles.image}
+                    src="/no-image.png"
+                    alt="No Image"
+                    width={1200}
+                    height={630}
+                  />
+                )}
                 <dl className={styles.content}>
                   <dt className={styles.title}>{article.title}</dt>
                   <dd className={styles.meta}>
